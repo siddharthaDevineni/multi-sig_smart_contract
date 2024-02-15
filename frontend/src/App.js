f6f2e9763b29df98a9b7da6a8a048c50e1dcd482
siddharthaDevineni <siddhartha.devineni@gmail.com>
2024-02-15 21:56:57 +0100

    basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,73 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import ABI from "./contracts/MultiSig.json";
+import { ethers, formatEther } from "ethers";
+
+const multisigAddress = "0xfA9349fe6A4DC98435D8067cC3a59298243D9453";
+const provider = new ethers.BrowserProvider(window.ethereum);
 
 function App() {
+  // functions to add in the UI
+  // state variable for storing the owner
+  // state variables for entering recipient and the amount to send
+  // state variable to send some ETH to the contract as a pool from different owners
+  // submit a transaction from an owner
+  // confirm the above transaction by an other owner
+  // check the funds in the contract before and after
+  // check the funds of the recipient before and after
+
+  const [owner, setOwner] = useState();
+  const [ownerBalance, setOwnerBalance] = useState();
+  const [fundTheContract, setFundTheContract] = useState();
+  const [beneficiary, setBeneficiary] = useState();
+  const [amountToSend, setAmountToSend] = useState();
+  const [userInfo, setUserInfo] = useState();
+  const [contractBalance, setContractBalance] = useState();
+
+  const checkWalletIsConnected = async () => {
+    if (provider) {
+      try {
+        await provider
+          .send("eth_requestAccounts", [])
+          .then(async () => await accountChangeHandler(provider.getSigner()));
+      } catch (error) {}
+    } else {
+      alert(
+        `You must install Metamask extension, a virtual Ethereum wallet from ${`https://metamask.io/download.html`}`
+      );
+    }
+  };
+
+  const accountChangeHandler = async (accountChanged) => {
+    setOwner(accountChanged);
+    setOwnerBalance(formatEther(await provider.getBalance(accountChanged)));
+  };
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header className="App-header">
+        <h2>Welcome to the Multi-Signature Decentraziled App</h2>
+        <p>Connected owner address:</p>
+        <p>Contract balance: </p>
+        <button>Fund the contract</button>
+        <p>Beneficiary address: </p>
+        <input
+          type="text"
+          placeholder="Enter your beneficiary address."
+          onChange={(e) => setBeneficiary(e.target.value)}
+        />
+        <span>----------------------------</span>
+        <p>Enter the amount in ETH</p>
+        <input
+          type="text"
+          placeholder="Enter the amount to transfer to the beneficiary."
+          onChange={(e) => setAmountToSend(e.target.value)}
+        ></input>
+      </header>
     </div>
   );
 }
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,2 @@
+// deployed address on goerli
+export const multisigAddress = "0xfA9349fe6A4DC98435D8067cC3a59298243D9453";
